eb4cb7d9aa77dd56cdb8ec61b96e09f2d3d7edfb
GauravC4 <gaurav.mrc@gmail.com>
2017-03-06 04:14:42 +0530

    navigator

---
+++ app/src/main/java/com/mrc/gaurav/asdf/Exam_Details.java
@@ -0,0 +1,29 @@
+package com.mrc.gaurav.asdf;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Exam_Details extends Fragment {
+
+
+    public Exam_Details() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exam__details, container, false);
+    }
+
+}
--- app/src/main/java/com/mrc/gaurav/asdf/LoginActivity.java
+++ app/src/main/java/com/mrc/gaurav/asdf/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.mrc.gaurav.asdf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = (Button)findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navigator = new Intent(LoginActivity.this, Navigator.class);
+                startActivity(navigator);
+                finish();
+            }
+        });
     }
 }
